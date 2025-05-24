2888371ee8646703841d85630d27d783ce94d1c5
Matheus Amaral <matheus.ratto11@gmail.com>
2025-05-24 11:43:57 -0300

    feat: adição da logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,41 +1,41 @@
 <template>
-    <nav class="navbar navbar-expand-lg bg-white border-bottom py-3">
-      <div class="container">
-        <a class="navbar-brand fw-bold text-dark" href="#">
-          <i class="bi bi-caret-down-fill me-1"></i>FASHION
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-          <ul class="navbar-nav gap-4">
-            <li class="nav-item"><a class="nav-link text-dark fw-semibold" href="#">CATALOGUE</a></li>
-            <li class="nav-item"><a class="nav-link text-dark fw-semibold" href="#">FASHION</a></li>
-            <li class="nav-item"><a class="nav-link text-dark fw-semibold" href="#">FAVOURITE</a></li>
-            <li class="nav-item"><a class="nav-link text-dark fw-semibold" href="#">LIFESTYLE</a></li>
-          </ul>
+  <nav class="navbar navbar-expand-lg bg-white border-bottom py-3">
+    <div class="container">
+      <a class="navbar-brand fw-bold text-dark" href="#">
+        <div class="d-flex align-items-center gap-2">
+          <img :src="logo" alt="Logo" style="height: 24px;" />
+          <span class="fw-bold">FASHION</span>
         </div>
-        <div class="d-none d-lg-block">
-          <button class="btn btn-dark rounded-pill px-4 fw-semibold">SIGN UP</button>
-        </div>
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+        <ul class="navbar-nav gap-4">
+          <li class="nav-item"><a class="nav-link text-dark fw-semibold" href="#">CATALOGUE</a></li>
+          <li class="nav-item"><a class="nav-link text-dark fw-semibold" href="#">FASHION</a></li>
+          <li class="nav-item"><a class="nav-link text-dark fw-semibold" href="#">FAVOURITE</a></li>
+          <li class="nav-item"><a class="nav-link text-dark fw-semibold" href="#">LIFESTYLE</a></li>
+        </ul>
       </div>
-    </nav>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Navbar'
-  }
-  </script>
-  
-  <style scoped>
-  .navbar-nav .nav-link:hover {
-    color: #f39c12;
-  }
-  </style>
-  
+      <div class="d-none d-lg-block">
+        <button class="btn btn-dark rounded-pill px-4 fw-semibold">SIGN UP</button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import logo from '../assets/imagens/vetor_logo.png'
+</script>
+
+<style scoped>
+.navbar-nav .nav-link:hover {
+  color: #f39c12;
+}
+</style>
