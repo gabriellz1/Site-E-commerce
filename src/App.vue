2f90dd5cababc97d7cbf6ff0f3dc5b570ae35122
Matheus Amaral <matheus.ratto11@gmail.com>
2025-05-24 10:11:36 -0300

    create: Criação do projeto e seus arquivos

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import Navbar from './components/NavBar.vue'
+import HeroBanner from './components/HeroBanner.vue'
+import BrandStrip from './components/BrandStrip.vue'
+import NewArrivals from './components/NewArrivals.vue'
+
+export default {
+  components: {
+    Navbar,
+    HeroBanner,
+    BrandStrip,
+    NewArrivals
+  }
+}
+</script>
+
+<template>
+  <div>
+    <Navbar />
+    <HeroBanner />
+    <BrandStrip />
+    <NewArrivals />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
